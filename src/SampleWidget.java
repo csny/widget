3c697be549b3dcd04918e003ef1c3dde99148563
csny <mac@macbook-no-MacBook-Pro.local>
2016-03-28 23:48:52 +0900

    アップロード

---
+++ src/SampleWidget.java
@@ -0,0 +1,40 @@
+package com.example.appsdk_widget;
+
+import java.util.Calendar;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+public class SampleWidget extends AppWidgetProvider {
+
+	@Override
+    public void onUpdate(Context c, AppWidgetManager awm, int[] awi) {
+		
+		// printdateに日付を入れる
+		Calendar cal = Calendar.getInstance();
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH);
+		int day = cal.get(Calendar.DAY_OF_MONTH);
+		String dayofweek = "";
+		switch (cal.get(Calendar.DAY_OF_WEEK)) {
+		case Calendar.SUNDAY: dayofweek = "(日)";
+		case Calendar.MONDAY: dayofweek = "(月)";
+		case Calendar.TUESDAY: dayofweek = "(火)";
+		case Calendar.WEDNESDAY: dayofweek = "(水)";
+		case Calendar.THURSDAY: dayofweek = "(木)";
+		case Calendar.FRIDAY: dayofweek = "(金)";
+		case Calendar.SATURDAY: dayofweek = "(土)";
+		}
+		String printdate = "平成" + (year-1988) + "年/"
+		+ year + "年\n" + (month+1) + "月" + day + "日　" + dayofweek;
+        //～　リモートビューにmain.xmlをセット　～//
+        RemoteViews rv = new RemoteViews(c.getPackageName(), R.layout.main);
+        rv.setTextViewText(R.id.textView1, printdate);
+        //～　ウィジェットにremoteViewsをセット　～//
+        ComponentName cn = new ComponentName(c, SampleWidget.class);
+        awm.updateAppWidget(cn, rv);
+    }
+}
